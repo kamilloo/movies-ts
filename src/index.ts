c912919fc68e9f522a7cd459f6cc7f01e90b8939
kamilloo <kamil.pietka85@gmail.com>
2023-03-01 18:32:51 +0100

    add env example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!')
+import express, { Express, Request, Response } from "express";
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app:Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req:Request, res:Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://172.0.0.1:${port}`);
+});
